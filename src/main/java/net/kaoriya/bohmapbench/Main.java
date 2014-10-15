0367659325ff7b23b5831dcefffe9929ced30acd
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-10-15 14:54:08 +0900

    first simple benchmark

--- src/main/java/net/kaoriya/bohmapbench/Main.java
+++ src/main/java/net/kaoriya/bohmapbench/Main.java
@@ -5,11 +5,15 @@ import com.cfelde.bohmap.Binary;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class Main {
 
     public static class Param {
+        public int warmUp;
         public int iteration;
         public int numOfItems;
         public double hitRate;
@@ -17,11 +21,6 @@ public class Main {
         public int valueMaxLen;
 
         public int bohmapPartitionCount;
-    }
-
-    public static class Result {
-        public long elapasedNanoTime;
-        public double hitRate;
 
         @Override
         public String toString() {
@@ -30,8 +29,33 @@ public class Main {
         }
     }
 
+    public static class Result {
+        public long elapasedNanoTime;
+        public int queryCount;
+        public int hitCount;
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("elapasedNanoTime",
+                        String.format("%1$,3d", elapasedNanoTime))
+                .append("QPS", String.format("%1$,3d", getQPS()))
+                .append("hitRate", getHitRate())
+                .toString();
+        }
+
+        public double getHitRate() {
+            return (double)this.hitCount / this.queryCount;
+        }
+
+        public long getQPS() {
+            return queryCount * (long)1e9 / this.elapasedNanoTime;
+        }
+    }
+
     public static void main(String[] args) {
         Param p = new Param();
+        p.warmUp = 4;
         p.iteration = 1000;
         p.numOfItems = 1000 * 1000;
         p.hitRate = 0.75;
@@ -39,24 +63,84 @@ public class Main {
         p.valueMaxLen = 128;
         p.bohmapPartitionCount = (int)(p.numOfItems * 1.5);
 
-        System.out.println("HashMap");
-        Result r1 = run(p, new HashMap<Binary, Binary>());
-        System.out.println(r1.toString());
-        System.gc();
+        System.out.println();
+        System.out.println("Param: " + p.toString());
 
-        System.out.println("BOHMap");
-        Result r2 = run(p, new BOHMap(p.bohmapPartitionCount));
-        System.out.println(r2.toString());
+        runHashMap(p);
+        runBOHMap(p);
+    }
+
+    public static Result runHashMap(Param p) {
         System.gc();
+        System.out.println();
+        System.out.println("HashMap");
+        Result r = run(p, new HashMap<Binary, Binary>());
+        System.out.println(r.toString());
+        System.gc();
+        return r;
+    }
+
+    public static Result runBOHMap(Param p) {
+        System.gc();
+        System.out.println();
+        System.out.println("BOHMap");
+        Result r = run(p, new BOHMap(p.bohmapPartitionCount));
+        System.out.println(r.toString());
+        System.gc();
+        return r;
     }
 
     public static Result run(Param p, Map<Binary, Binary> m) {
-        Result r = new Result();
+        return run(p, m, new Random());
+    }
+
+    public static Result run(Param p, Map<Binary, Binary> m, Random r) {
+        // Generate keys.
+        ArrayList<Binary> keys = new ArrayList<>();
+        int keySize = (int)(p.numOfItems / p.hitRate + 0.5);
+        for (int i = 0; i < keySize; ++i) {
+            keys.add(randomBinary(r, p.keyMaxLen));
+        }
+
+        // Setup map.
+        for (int i = 0; i < p.numOfItems; ++i) {
+            m.put(keys.get(i), randomBinary(r, p.valueMaxLen));
+        }
+
+        // Warm up.
+        for (int i = 0; i < p.warmUp; ++i) {
+            run(p, m, r, keys);
+        }
+
+        // Query to map.
+        return run(p, m, r, keys);
+    }
+
+    public static Result run(Param p, Map<Binary, Binary> m, Random r,
+            ArrayList<Binary> keys) {
+        Result res = new Result();
+        int keySize = keys.size();
         long startAt = System.nanoTime();
 
-        // TODO:
+        // Query to map.
+        int query = 0, hit = 0;
+        for (int i = 0; i < p.iteration; ++i) {
+            ++query;
+            Binary v = m.get(keys.get(r.nextInt(keySize)));
+            if (v != null) {
+                ++hit;
+            }
+        }
 
-        r.elapasedNanoTime = System.nanoTime() - startAt;
-        return r;
+        res.elapasedNanoTime = System.nanoTime() - startAt;
+        res.queryCount = query;
+        res.hitCount = hit;
+        return res;
+    }
+
+    public static Binary randomBinary(Random r, int len) {
+        byte[] b = new byte[len];
+        r.nextBytes(b);
+        return new Binary(b);
     }
 }
